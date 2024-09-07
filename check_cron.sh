db2e26ec51ee9b78723e1dc3663a8f6121646b68
Mr.Bai <1507820372@qq.com>
2024-09-07 09:08:52 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -22,7 +22,7 @@ else
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_NEZHA}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_NEZHA}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") | crontab -
-    (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * ${CRON_C}") | crontab -
+    (crontab -l | grep -F "* * pgrep -x \"s6\" > /dev/null") || (crontab -l; echo "*/12 * * * * ${CRON_C}") | crontab -
   elif [ -e "${WORKDIR}/start.sh" ]; then
     echo "添加 nezha 的 crontab 重启任务"
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}") | crontab -
@@ -32,6 +32,6 @@ else
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") | crontab -
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") ||(crontab -l; echo "@reboot ${CRON_C}") | crontab -
-    (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * ${CRON_C}") | crontab -
+    (crontab -l | grep -F "* * pgrep -x \"s6\" > /dev/null") || (crontab -l; echo "*/12 * * * * ${CRON_C}") | crontab -
   fi
 fi
